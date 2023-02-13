12ee678e508ab8ad47a9aa52fc785c9b5d93cedb
jrobinson57 <goalie101010@gmail.com>
2023-02-13 00:00:02 -0500

    Initial Setup

--- src/main/java/bruteforce/Main.java
+++ src/main/java/bruteforce/Main.java
@@ -1,9 +1,38 @@
 package bruteforce;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello There.");
+        Main m = new Main();
+        Scanner sc = new Scanner(System.in);
+        String filepath = "";
+
+        System.out.println("Enter the filepath of the input.");
+        filepath = sc.nextLine();
+        m.readInput(filepath);
+    }
+
+    public void readInput(String filepath)
+    {
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(filepath));
+            String line;
+            while((line = reader.readLine()) != null)
+            {
+                System.out.println(line);
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("Wrong input");
+            e.printStackTrace();
+        }
     }
 }
