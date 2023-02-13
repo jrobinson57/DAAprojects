fcab3bf5d446129c54efcf1d926655e1d6b968f1
jrobinson57 <goalie101010@gmail.com>
2023-02-13 00:44:02 -0500

    Attempted to read data and store in arraylist

--- src/main/java/bruteforce/Main.java
+++ src/main/java/bruteforce/Main.java
@@ -3,10 +3,15 @@ package bruteforce;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main
 {
+
+    String inputData;
+    ArrayList<Integer> array = new ArrayList<Integer>();
+
     public static void main(String[] args)
     {
         Main m = new Main();
@@ -18,7 +23,33 @@ public class Main
         m.readInput(filepath);
     }
 
+    //reads file input and attempts to store it in an arrayList
+    // string filepath - source of input file
     public void readInput(String filepath)
+    {
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(filepath));
+            String line;
+            while((reader.ready()))
+            {
+                for(int i = 0; i < 1; i++)
+                {
+                    array.add(reader.read());
+                }
+            }
+            System.out.println(array);
+        }
+        catch(IOException e)
+        {
+            System.out.println("Wrong input");
+            e.printStackTrace();
+        }
+    }
+
+    //prints file input
+    //@string filepath - source of input file
+    public void printInput(String filepath)
     {
         try
         {
@@ -26,7 +57,7 @@ public class Main
             String line;
             while((line = reader.readLine()) != null)
             {
-                System.out.println(line);
+                System.out.println();
             }
         }
         catch(IOException e)
@@ -34,5 +65,10 @@ public class Main
             System.out.println("Wrong input");
             e.printStackTrace();
         }
+    }
+
+    public void parseInput(String inputData)
+    {
+        //while()
     }
 }
